625338330e6eb2efe5a95dcaa163f756cfb9c819
Ashley Chapokas <ashleychapokas@Ashley-Chapokass-MacBook.local>
2013-10-12 13:35:53 -0400

    first commit

---
+++ bookies/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookies::Application.config.secret_token = '791fdc2ed7cb18ee2ce86aa9603ece51b3d7f270c8a60c43f5d67effe63f22bc63c7c072a9344a0ce5d9fdef5503551c4d26af06c865e0cb2cfc2f2a172f59aa'
